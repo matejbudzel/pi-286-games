e4688b78f841952f6cc4f85fc8e8f6ff37e338d0
Matej Budzel <budzel@gmail.com>
2026-08-29 21:00:46 +0200

    Install stream presenter with isolated sudo prefix

--- scripts/deploy-stream-presenter-to-pi.sh
+++ scripts/deploy-stream-presenter-to-pi.sh
@@ -9,8 +9,9 @@ target=${PI286_STREAM_TARGET:-pi286}
 proxmox=${PI286_STREAM_PROXMOX:-proxmox}
 container=${PI286_STREAM_CONTAINER:-112}
 binary=${STREAM_PRESENTER_OUTPUT:-$repo/dist/pi286-stream-presenter-rpi1-armv6-armhf}
-ssh "$target" 'mkdir -p ~/.config /opt/pi286/stream/bin'
-scp "$binary" "$target":/opt/pi286/stream/bin/pi286-stream-presenter
+ssh "$target" 'mkdir -p ~/.config; sudo -n install -d -m 0755 /opt/pi286/stream/bin'
+scp "$binary" "$target":/tmp/pi286-stream-presenter
+ssh "$target" 'sudo -n install -m 0755 /tmp/pi286-stream-presenter /opt/pi286/stream/bin/pi286-stream-presenter; rm -f /tmp/pi286-stream-presenter'
 ssh "$proxmox" "pct exec $container -- cat /etc/pi286-stream.token" | ssh "$target" 'umask 077; cat > ~/.config/pi286-stream.token'
-ssh "$target" 'chmod 700 /opt/pi286/stream/bin/pi286-stream-presenter; chmod 600 ~/.config/pi286-stream.token'
+ssh "$target" 'chmod 600 ~/.config/pi286-stream.token'
 echo "Presenter deployed. Set dosbox_backend=auto and remote_dosbox_url=http://192.168.100.194:28680 in config/host.conf."
